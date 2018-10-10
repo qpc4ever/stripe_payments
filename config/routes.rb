f598ebfc83c132cd9c89e6a22e293e204e758d64
qpc4ever <qpc4ever@gmail.com>
2018-10-10 14:06:12 -0400

    added purchase storing and display funcionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :charges
+  resources :purchases, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
