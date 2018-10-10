dc5e7ead2b59377d132c1e9be6ada01c28d61875
qpc4ever <qpc4ever@gmail.com>
2018-10-10 13:22:37 -0400

    cleaned up some code

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,8 +3,7 @@ class ChargesController < ApplicationController
     end
 
     def create
-        # Amount in cents
-        @amount = 500
+       
       
         customer = Stripe::Customer.create(
           :email => params[:stripeEmail],
@@ -13,8 +12,8 @@ class ChargesController < ApplicationController
       
         charge = Stripe::Charge.create(
           :customer    => customer.id,
-          :amount      => @amount,
-          :description => 'Rails Stripe customer',
+          :amount      => params[:amount],
+          :description => 'Growth Hacking Crash Course',
           :currency    => 'usd'
         )
       
